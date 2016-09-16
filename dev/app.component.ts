19acfac63744a4e3a8951bc5e6f7ec1607b63dbd
BenMacLean <benhmaclean@gmail.com>
2016-09-16 13:38:09 -0300

    startingpoint

---
+++ dev/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <header>
+            <h1>geoGramJS</h1>
+        </header>
+    `,
+})
+export class AppComponent {
+}
